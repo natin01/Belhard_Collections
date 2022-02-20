1c42e5620e86eacd118e8e83bf919d88af29aa90
natin <natin.cher@gmail.com>
2022-02-20 22:48:25 +0300

    added error handling

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-a = int(input())
-b = int(input())
-c = (a**2+b**2)**0.5
-S = a*b/2
-print(f"Треугольник 1 с катетами {a} и {b} имеет площадь {S} и гипотенузу {c}")
+# a = int(input())
+# b = int(input())
+# c = (a**2+b**2)**0.5
+# S = a*b/2
+# print(f"Треугольник 1 с катетами {a} и {b} имеет площадь {S} и гипотенузу {c}")
+
+katet_str1 = input("Введите первые катеты а:")
+katet_str2 = input("Введите вторые катеты b:")
+katet_list1 = katet_str1.split()
+katet_list2 = katet_str2.split()
+
+if len(katet_list1) != len(katet_list2):
+    raise RuntimeError("InconsistentDataError")
+
+print(katet_list1)
+print(len(katet_list1))
+
+for i in range(len(katet_list1)):
+    print(f"Треугольник {i+1} c катетами {katet_list1[i]} и {katet_list2[i]}")
