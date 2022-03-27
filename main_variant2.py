3421915fc809168aee8d921a4e8485eb6e245185
natin <natin.cher@gmail.com>
2022-03-27 21:55:15 +0300

    added katet homework

--- main_variant2.py
+++ main_variant2.py
@@ -4,24 +4,27 @@
 # S = a*b/2
 # print(f"Треугольник 1 с катетами {a} и {b} имеет площадь {S} и гипотенузу {c}")
 
-katet_str1 = input("Введите первые катеты а:")
-katet_str2 = input("Введите вторые катеты b:")
-katet_list1 = katet_str1.split()
-katet_list2 = katet_str2.split()
+katet_str = input("Введите катеты:")
+katet_list = katet_str.split()
 
-if len(katet_list1) != len(katet_list2):
+#katet_list1 = (katet_list[0, len(katet_list), 2])
+#katet_list2 = (katet_list[1, len(katet_list), 2])
+
+if len(katet_list) % 2 != 0:
     raise RuntimeError("InconsistentDataError")
 
-print(katet_list1)
-print(len(katet_list1))
-
-for i in range(len(katet_list1)):
+print(katet_list)
+print(len(katet_list))
+i = 0
+cnt = 1
+while i < len(katet_list):
     try:
-        S = int(katet_list1[i]) * int(katet_list2[i]) / 2
+        S = int(katet_list[i]) * int(katet_list[i+1]) / 2
     except:
         raise RuntimeError("NonNumericError")
     else:
-        c = (int(katet_list1[i])**2 + int(katet_list2[i])**2)**0.5
-        print(f"Треугольник {i+1} c катетами {katet_list1[i]} и {katet_list2[i]} имеет площадь {S} и гипотенузу {c}")
-
+        c = (int(katet_list[i])**2 + int(katet_list[i+1])**2)**0.5
+        print(f"Треугольник {cnt} c катетами {katet_list[i]} и {katet_list[i+1]} имеет площадь {S} и гипотенузу {c}")
+        i = i+2
+        cnt = cnt + 1
 
